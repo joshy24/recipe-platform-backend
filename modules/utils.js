abb69dca4d8011434e9664905f6f664835060dd2
joshy24 <joshuamajebi@yahoo.com>
2022-10-06 17:20:39 +0100

    worked on default units and conversions

--- modules/utils.js
+++ modules/utils.js
@@ -9,25 +9,21 @@ module.exports.defaultUnitsAndConversions =  [
         name: "Kilogram",
         abbreviation: "kg",
         amount: 1,
-        unit: [
-            {
+        unit: {
                 name: "Gram",
                 abbreviation: "g",
                 amount: 1000
             }
-        ]
     },
     {
         name: "Litre",
         abbreviation: "L",
         amount: 1,
-        unit: [
-            {
+        unit: {
                 name: "Millilitre",
                 abbreviation: "ml",
                 amount: 1000
-            }
-        ]
+        }
     }
 ]
 
