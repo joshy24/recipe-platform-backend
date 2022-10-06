d0435c229de3eed8ded190fa9b86d95787fa459f
joshy24 <joshuamajebi@yahoo.com>
2022-10-06 15:14:27 +0100

    worked on default units and conversions

--- modules/utils.js
+++ modules/utils.js
@@ -4,28 +4,30 @@ module.exports.getPriceOfQuantity = (originalPrice, originalQuantity, requiredQu
     return ( originalPrice * requiredQuantity ) / originalQuantity
 }
 
-module.exports.defaultUnitsAndConversions = () => [
-    {
-        name: "Kilogram",
-        abbreviation: "kg",
-        amount: 1,
-        unit: [
-            {
-                name: "g",
-                amount: 1000
-            }
-        ]
-    },
-    {
-        name: "Litre",
-        abbreviation: "L",
-        amount: 1,
-        unit: [
-            {
-                name: "g",
-                amount: 1000
-            }
-        ]
-    }
-]
+module.exports.defaultUnitsAndConversions = () => { 
+    return [
+                {
+                    name: "Kilogram",
+                    abbreviation: "kg",
+                    amount: 1,
+                    unit: [
+                        {
+                            name: "g",
+                            amount: 1000
+                        }
+                    ]
+                },
+                {
+                    name: "Litre",
+                    abbreviation: "L",
+                    amount: 1,
+                    unit: [
+                        {
+                            name: "g",
+                            amount: 1000
+                        }
+                    ]
+                }
+            ]
+}
 
